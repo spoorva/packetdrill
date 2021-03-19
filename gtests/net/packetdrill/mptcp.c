spoorva/packetdrill

commit 58039d6f348fe625c115c27c12080227dfdb0bac

packetdrill: fix a couple of errors when parsing ADD_ADDR with ports
- fix typo causing bogus assignment of port / HMACs while parsing
  ADD_ADDR (v4) with port
- fix hardcoded port 0 in the call to add_addr_ipv{4,6}_hmac()

Signed-off-by: Davide Caratti <dcaratti@redhat.com>

gtests/net/packetdrill/mptcp.c
@@ -2053,7 +2053,7 @@ int mptcp_subtype_add_address(struct packet *packet_to_modify,
                                                            mp_state.packetdrill_key,
                                                            add_addr_live->data.add_addr.address_id,
                                                            &add_addr_live->data.add_addr.ipv4_w_port_hmac.ipv4,
-                                                           0);
+                                                           ntohs(add_addr_live->data.add_addr.ipv4_w_port_hmac.port));
 		}else if(add_addr_script->length == TCPOLEN_ADD_ADDR_V6){
 			add_addr_script->data.add_addr.ipv6 = endpoint->ip.ip.v6;
 		}else if(add_addr_script->length == TCPOLEN_ADD_ADDR_V6_PORT){
@@ -2079,7 +2079,7 @@ int mptcp_subtype_add_address(struct packet *packet_to_modify,
                                                            mp_state.packetdrill_key,
                                                            add_addr_live->data.add_addr.address_id,
                                                            &add_addr_live->data.add_addr.ipv6_w_port_hmac.ipv6,
-                                                           0);
+                                                           ntohs(add_addr_live->data.add_addr.ipv6_w_port_hmac.port));
 		}else{
 			return STATUS_ERR;
 		}

gtests/net/packetdrill/parser.y
@@ -2249,8 +2249,8 @@ tcp_option
                 // ipv4 + port + hmac
                 }else if($4 != UNDEFINED && ($5.auto_conf || $5.hash != UNDEFINED)){
                         $$ = tcp_option_new(TCPOPT_MPTCP, TCPOLEN_ADD_ADDR_V4_PORT_HMAC);
-			$$->data.add_addr.ipv4_w_port.port = htons($4);
-                        $$->data.add_addr.ipv4_w_hmac.hmac = htobe64($5.hash);
+			$$->data.add_addr.ipv4_w_port_hmac.port = htons($4);
+                        $$->data.add_addr.ipv4_w_port_hmac.hmac = htobe64($5.hash);
 		}
 	// ipv4
 	}else if($3 == AF_INET && $4 == UNDEFINED && !$5.auto_conf && $5.hash == UNDEFINED){
@@ -2268,8 +2268,8 @@ tcp_option
         // ipv4 + port + hmac
         }else if($3 == AF_INET && $4 != UNDEFINED && ($5.auto_conf || $5.hash != UNDEFINED)){
                 $$ = tcp_option_new(TCPOPT_MPTCP, TCPOLEN_ADD_ADDR_V4_PORT_HMAC);
-		$$->data.add_addr.ipv4_w_port.port = htons($4);
-                $$->data.add_addr.ipv4_w_hmac.hmac = htobe64($5.hash);
+		$$->data.add_addr.ipv4_w_port_hmac.port = htons($4);
+                $$->data.add_addr.ipv4_w_port_hmac.hmac = htobe64($5.hash);
 	// ipv6
 	}else if($3 == AF_INET6 && $4 == UNDEFINED && !$5.auto_conf && $5.hash == UNDEFINED){
 		$$ = tcp_option_new(TCPOPT_MPTCP, TCPOLEN_ADD_ADDR_V6);

commit 93520fc119ae62133ad85f532a4f6adc18756acc

fix inbound HMAC for ADD_ADDR with port
port was still hardcoded to 0

Fixes: 58039d6f348f ("packetdrill: fix a couple of errors when parsing ADD_ADDR with ports")

gtests/net/packetdrill/mptcp.c
@@ -1991,7 +1991,7 @@ int mptcp_subtype_add_address(struct packet *packet_to_modify,
                                                            mp_state.kernel_key,
                                                            add_addr_script->data.add_addr.address_id,
                                                            &add_addr_script->data.add_addr.ipv4_w_port_hmac.ipv4,
-                                                           0);
+                                                           ntohs(add_addr_script->data.add_addr.ipv4_w_port_hmac.port));
 		}else if(add_addr_live->length == TCPOLEN_ADD_ADDR_V6){
 			add_addr_live->data.add_addr.ipv6 = endpoint->ip.ip.v6;
 		}else if(add_addr_live->length == TCPOLEN_ADD_ADDR_V6_PORT){
@@ -2017,7 +2017,7 @@ int mptcp_subtype_add_address(struct packet *packet_to_modify,
                                                            mp_state.kernel_key,
                                                            add_addr_script->data.add_addr.address_id,
                                                            &add_addr_script->data.add_addr.ipv6_w_port_hmac.ipv6,
-                                                           0);
+                                                           ntohs(add_addr_script->data.add_addr.ipv6_w_port_hmac.port));
 		}else
 			return STATUS_ERR;
 	}else if(direction == DIRECTION_OUTBOUND){